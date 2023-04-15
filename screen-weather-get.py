be91bd5d1f6603690dd1af85814332c9d5b49048
txbdev <57550673+txbdev@users.noreply.github.com>
2023-04-14 23:16:10 -0500

    Update screen-weather-get.py

--- screen-weather-get.py
+++ screen-weather-get.py
@@ -16,17 +16,95 @@ configure_logging()
 
 
 def format_weather_description(weather_description):
-    if len(weather_description) < 20:
+    if len(weather_description) < 18:
         return {1: weather_description, 2: ''}
 
-    splits = textwrap.fill(weather_description, 20, break_long_words=False,
+    splits = textwrap.fill(weather_description, 18, break_long_words=False,
                            max_lines=2, placeholder='...').split('\n')
     weather_dict = {1: splits[0]}
     weather_dict[2] = splits[1] if len(splits) > 1 else ''
     return weather_dict
 
 
-def get_weather(location_lat, location_long, units):
+def get_weather_current(location_lat, location_long, units):
+
+    # gather relevant environment configs
+    climacell_apikey = os.getenv("CLIMACELL_APIKEY")
+    openweathermap_apikey = os.getenv("OPENWEATHERMAP_APIKEY")
+    metoffice_clientid = os.getenv("METOFFICEDATAHUB_CLIENT_ID")
+    metoffice_clientsecret = os.getenv("METOFFICEDATAHUB_CLIENT_SECRET")
+    accuweather_apikey = os.getenv("ACCUWEATHER_APIKEY")
+    accuweather_locationkey = os.getenv("ACCUWEATHER_LOCATIONKEY")
+    metno_self_id = os.getenv("METNO_SELF_IDENTIFICATION")
+    visualcrossing_apikey = os.getenv("VISUALCROSSING_APIKEY")
+    use_met_eireann = os.getenv("WEATHER_MET_EIREANN")
+    weathergov_self_id = os.getenv("WEATHERGOV_SELF_IDENTIFICATION")
+    smhi_self_id = os.getenv("SMHI_SELF_IDENTIFICATION")
+
+    if (
+        not climacell_apikey
+        and not openweathermap_apikey
+        and not metoffice_clientid
+        and not accuweather_apikey
+        and not metno_self_id
+        and not visualcrossing_apikey
+        and not use_met_eireann
+        and not weathergov_self_id
+        and not smhi_self_id
+    ):
+        logging.error("No weather provider has been configured (Climacell, OpenWeatherMap, Weather.gov, MetOffice, AccuWeath>        sys.exit(1)
+
+    if visualcrossing_apikey:
+        logging.info("Getting weather from Visual Crossing")
+        weather_provider = visualcrossing.VisualCrossing(visualcrossing_apikey, location_lat, location_long, units)
+
+    elif use_met_eireann:
+        logging.info("Getting weather from Met Eireann")
+        weather_provider = meteireann.MetEireann(location_lat, location_long, units)
+
+    elif weathergov_self_id:
+        logging.info("Getting weather from Weather.gov")
+        weather_provider = weathergov.WeatherGov(weathergov_self_id, location_lat, location_long, units)
+
+    elif metno_self_id:
+        logging.info("Getting weather from Met.no")
+        weather_provider = metno.MetNo(metno_self_id, location_lat, location_long, units)
+
+    elif accuweather_apikey:
+        logging.info("Getting weather from Accuweather")
+        weather_provider = accuweather.AccuWeather(accuweather_apikey, location_lat,
+                                                   location_long,
+                                                   accuweather_locationkey,
+                                                   units)
+
+    elif metoffice_clientid:
+        logging.info("Getting weather from Met Office Weather Datahub")
+        weather_provider = metofficedatahub.MetOffice(metoffice_clientid,
+                                                      metoffice_clientsecret,
+                                                      location_lat,
+                                                      location_long,
+                                                      units)
+
+    elif openweathermap_apikey:
+        logging.info("Getting weather from OpenWeatherMap")
+        weather_provider = openweathermap.OpenWeatherMap(openweathermap_apikey,
+                                                         location_lat,
+                                                         location_long,
+                                                         units)
+
+    elif climacell_apikey:
+        logging.info("Getting weather from Climacell")
+        weather_provider = climacell.Climacell(climacell_apikey, location_lat, location_long, units)
+
+    elif smhi_self_id:
+        logging.info("Getting weather from SMHI")
+        weather_provider = smhi.SMHI(smhi_self_id, location_lat, location_long, units)
+
+    weather_current = weather_provider.get_weather_current()
+    logging.info("weather - {}".format(weather_current))
+    return weather_current
+
+def get_weather_forecast(location_lat, location_long, units):
 
     # gather relevant environment configs
     climacell_apikey = os.getenv("CLIMACELL_APIKEY")
@@ -101,14 +179,12 @@ def get_weather(location_lat, location_long, units):
         logging.info("Getting weather from SMHI")
         weather_provider = smhi.SMHI(smhi_self_id, location_lat, location_long, units)
 
-    weather = weather_provider.get_weather()
-    logging.info("weather - {}".format(weather))
-    return weather
-
+    weather_forecast = weather_provider.get_weather_forecast()
+    logging.info("weather - {}".format(weather_forecast))
+    return weather_forecast
 
 def format_alert_description(alert_message):
     return html.escape(alert_message)
-
 
 def get_alert_message(location_lat, location_long):
     alert_message = ""
@@ -134,7 +210,6 @@ def get_alert_message(location_lat, location_long):
     logging.info("alert - {}".format(alert_message))
     return alert_message
 
-
 def main():
 
     template_name = os.getenv("SCREEN_LAYOUT", "1")
@@ -149,31 +224,51 @@ def main():
         units = "imperial"
         degrees = "°F"
 
-    weather = get_weather(location_lat, location_long, units)
+    weather_current = get_weather_current(location_lat, location_long, units)
 
-    if not weather:
+    if not weather_current:
         logging.error("Unable to fetch weather payload. SVG will not be updated.")
         return
 
-    weather_desc = format_weather_description(weather["description"])
+    weather_desc_current = format_weather_description(weather_current["current_description"])
+
+    weather_forecast = get_weather_forecast(location_lat, location_long, units)
+
+    if not weather_forecast:
+        logging.error("Unable to fetch weather payload. SVG will not be updated.")
+        return
+
+    weather_desc_forecast = format_weather_description(weather_forecast["forecast_description"])
 
     alert_message = get_alert_message(location_lat, location_long)
     alert_message = format_alert_description(alert_message)
 
     time_now = get_formatted_time(datetime.datetime.now())
-    time_now_font_size = "100px"
+    logging.info(time_now)
+    time_now_font_size = "140px"
 
     if len(time_now) > 6:
         time_now_font_size = str(100 - (len(time_now)-5) * 5) + "px"
+        
+    daytime = weather_forecast['is_it_daytime']
+    if daytime == True:
+        today_tonight = "Today:"
+    else:
+        today_tonight = "Tonight:"
 
     output_dict = {
-        'LOW_ONE': "{}{}".format(str(round(weather['temperatureMin'])), degrees),
-        'HIGH_ONE': "{}{}".format(str(round(weather['temperatureMax'])), degrees),
-        'ICON_ONE': weather["icon"],
-        'WEATHER_DESC_1': weather_desc[1],
-        'WEATHER_DESC_2': weather_desc[2],
+        'TEMP_NOW': "{}{}".format(str(round(weather_current['current_temperature'])), degrees),
+        'ICON_ONE': weather_current["current_icon"],
+        'WEATHER_DESC_1': weather_desc_current[1],
+        'WEATHER_DESC_2': weather_desc_current[2],
+        'LOW_ONE': "{}{}".format(str(round(weather_forecast['forecast_temperatureMin'])), degrees),
+        'HIGH_ONE': "{}{}".format(str(round(weather_forecast['forecast_temperatureMax'])), degrees),
+        'ICON_TWO': weather_forecast["forecast_icon"],
+        'WEATHER_DESC_3': weather_desc_forecast[1],
+        'WEATHER_DESC_4': weather_desc_forecast[2],
         'TIME_NOW_FONT_SIZE': time_now_font_size,
         'TIME_NOW': time_now,
+        'TODAY_TONIGHT': today_tonight,
         'HOUR_NOW': datetime.datetime.now().strftime("%-I %p"),
         'DAY_ONE': datetime.datetime.now().strftime("%b %-d, %Y"),
         'DAY_NAME': datetime.datetime.now().strftime("%A"),
